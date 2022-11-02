bf5bf1040ac13a38b8d4463943dc9da8c21eef0a
ArneSamson <arnesamson007@gmail.com>
2022-11-02 15:02:21 +0100

    added component file

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import BookTile from './components/BookTile';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Juditheke je t'aime</Text>
       <StatusBar style="auto" />
   
     </View>
---
+++ components/BookTile.js
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const BookTile = (props) => {
+    return (
+        <View style={styles.tile}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    tile: {
+        flexBasis: '48%',
+        height: 220,
+        backgroundColor: '#',
+        marginVertical: 10,
+        borderWidth: 1,
+        marginHorizontal: 3,
+        borderColor: '#660066',
+    }
+
+}) 
+
+export default Booktile;
